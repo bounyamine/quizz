0f36685729f2b1778e59c527bd42ed73f750e324
bounyamine <ousmanoubounyamine@gmail.com>
2024-09-16 17:10:20 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,53 +1,45 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
 
 const App = () => {
   const [categories, setCategories] = useState([]);
   const [questions, setQuestions] = useState([]);
   const [questionIndex, setQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
-  const [highScore, setHighScore] = useState(() => parseInt(localStorage.getItem('HighScoreTrivia')) || 0);
-  const [selectedCategory, setSelectedCategory] = useState('');
+  const [highScore, setHighScore] = useState(
+    () => parseInt(localStorage.getItem("HighScoreTrivia")) || 0
+  );
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [questionAmount, setQuestionAmount] = useState(10);
-  const [difficulty, setDifficulty] = useState('');
+  const [difficulty, setDifficulty] = useState("");
   const [currentQuestion, setCurrentQuestion] = useState(null);
   const [showQuiz, setShowQuiz] = useState(false);
-  const [result, setResult] = useState('');
-  const [progress, setProgress] = useState('');
-  const [selectedAnswer, setSelectedAnswer] = useState('');
-  const [, setAnswerState] = useState('');
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [result, setResult] = useState("");
+  const [progress, setProgress] = useState("");
+  const [quizFinished, setQuizFinished] = useState(false);
   const questionStartTime = useRef(null);
 
   const baseScorePerQuestion = 1000;
   const penaltyPerSecond = 10;
 
+  const shuffleArray = (array) => {
+    return array.sort(() => Math.random() - 0.5);
+  };
+
   useEffect(() => {
     fetchCategories();
   }, []);
-
-  // Utilisation de useCallback pour mémoriser la fonction displayQuestion
-  const displayQuestion = useCallback(() => {
-    if (questionIndex < questions.length) {
-      setCurrentQuestion(questions[questionIndex]);
-      setProgress(`Question ${questionIndex + 1}/${questions.length}`);
-      setAnswerState('');
-      setSelectedAnswer('');
-      questionStartTime.current = Date.now();
-    } else {
-      updateHighScore();
-      setResult(`Quiz Finished! Your final score is ${score}`);
-    }
-  // eslint-disable-next-line no-use-before-define
-  }, [questionIndex, questions, score, updateHighScore]); // Ajout des dépendances pertinentes
 
   useEffect(() => {
     if (questions.length > 0 && questionIndex < questions.length) {
       displayQuestion();
     }
-  }, [questions, questionIndex, displayQuestion]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [questions, questionIndex]);
 
   const fetchCategories = async () => {
-    const response = await fetch('https://opentdb.com/api_category.php');
+    const response = await fetch("https://opentdb.com/api_category.php");
     const data = await response.json();
     setCategories(data.trivia_categories);
   };
@@ -55,6 +47,7 @@ const App = () => {
   const startGame = () => {
     fetchQuestions();
     setShowQuiz(true);
+    setQuizFinished(false); // Reset quiz state when starting a new game
   };
 
   const fetchQuestions = async () => {
@@ -70,34 +63,78 @@ const App = () => {
     setScore(0);
   };
 
+  const displayQuestion = () => {
+    if (questionIndex < questions.length) {
+      const shuffledAnswers = shuffleArray([
+        ...questions[questionIndex].incorrect_answers,
+        questions[questionIndex].correct_answer,
+      ]);
+      setCurrentQuestion({
+        ...questions[questionIndex],
+        shuffledAnswers,
+      });
+      setProgress(`Question ${questionIndex + 1}/${questions.length}`);
+      questionStartTime.current = Date.now();
+      setSelectedAnswer(null);
+    } else {
+      finishQuiz(); // End the quiz when questions are finished
+    }
+  };
+
   const selectAnswer = (answer) => {
-    setSelectedAnswer(answer);
     const correctAnswer = currentQuestion.correct_answer;
+    setSelectedAnswer(answer);
     const timeTaken = (Date.now() - questionStartTime.current) / 1000;
-    let scoreForThisQuestion = Math.max(baseScorePerQuestion - Math.floor(timeTaken) * penaltyPerSecond, 0);
+    let scoreForThisQuestion = Math.max(
+      baseScorePerQuestion - Math.floor(timeTaken) * penaltyPerSecond,
+      0
+    );
 
     if (answer === correctAnswer) {
       setScore(score + scoreForThisQuestion);
-      setAnswerState('correct');
       setResult(`Correct! + ${scoreForThisQuestion} Points`);
     } else {
-      setAnswerState('incorrect');
       setResult(`Wrong! The correct answer was: ${correctAnswer}`);
     }
 
     setTimeout(() => {
-      setQuestionIndex(questionIndex + 1);
-      setResult('');
+      if (questionIndex < questions.length - 1) {
+        setQuestionIndex(questionIndex + 1);
+      } else {
+        finishQuiz();
+      }
+      setResult("");
     }, 3000);
   };
 
-  const updateHighScore = useCallback(() => {
+  const finishQuiz = () => {
+    updateHighScore();
+    setQuizFinished(true); // Indicate that the quiz has finished
+    setResult(`Quiz Finished! Your final score is ${score}`);
+  };
+
+  const updateHighScore = () => {
     if (score > highScore) {
       setHighScore(score);
-      localStorage.setItem('HighScoreTrivia', score);
+      localStorage.setItem("HighScoreTrivia", score);
     }
-  }, [score, highScore]);
-  
+  };
+
+  const decodeHTMLEntities = (text) => {
+    var textArea = document.createElement("textarea");
+    textArea.innerHTML = text;
+    return textArea.value;
+  };
+
+  const restartGame = () => {
+    setQuestions([]);
+    setShowQuiz(false);
+    setScore(0);
+    setQuestionIndex(0);
+    setSelectedAnswer(null);
+    setResult("");
+    setQuizFinished(false);
+  };
 
   return (
     <div id="quiz-container">
@@ -144,6 +181,13 @@ const App = () => {
           </div>
           <button onClick={startGame}>Start Game</button>
         </div>
+      ) : quizFinished ? (
+        <div id="quiz-finished">
+          <h2>{result}</h2>
+          <p>Your final score: {score}</p>
+          <p>High Score: {highScore}</p>
+          <button onClick={restartGame}>Play Again</button>
+        </div>
       ) : (
         <div id="quiz">
           <div id="progress">{progress}</div>
@@ -151,20 +195,28 @@ const App = () => {
           <div id="highScore">High Score: {highScore}</div>
           {currentQuestion && (
             <>
-              <div id="question">{currentQuestion.question}</div>
+              <div id="question">
+                {decodeHTMLEntities(currentQuestion.question)}
+              </div>
               <div id="answers">
-                {[...currentQuestion.incorrect_answers, currentQuestion.correct_answer].sort().map(
-                  (answer, index) => (
-                    <button
-                      key={index}
-                      onClick={() => selectAnswer(answer)}
-                      className={`answer-btn ${selectedAnswer === answer ? (answer === currentQuestion.correct_answer ? 'correct' : 'incorrect') : ''}`}
-                      disabled={selectedAnswer !== ''}
-                    >
-                      {answer}
-                    </button>
-                  )
-                )}
+                {currentQuestion.shuffledAnswers.map((answer, index) => (
+                  <button
+                    key={index}
+                    className={`answer-btn ${
+                      selectedAnswer
+                        ? answer === currentQuestion.correct_answer
+                          ? "correct"
+                          : answer === selectedAnswer
+                          ? "incorrect"
+                          : ""
+                        : ""
+                    }`}
+                    onClick={() => selectAnswer(answer)}
+                    disabled={!!selectedAnswer}
+                  >
+                    {decodeHTMLEntities(answer)}
+                  </button>
+                ))}
               </div>
               <div id="result">{result}</div>
             </>
